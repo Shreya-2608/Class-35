0588066be6bb85bcbb05a5829bd40d3c94569743
Shreya-2608 <shreya.kalluri2608@gmail.com>
2021-02-26 15:03:41 +0530

    Modified

--- sketch.js
+++ sketch.js
@@ -1,29 +1,45 @@
-var ball;
+var Ball1;
+var database, position;
 
 function setup(){
+    database = firebase.database();
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    Ball1 = createSprite(250,250,10,10);
+    Ball1.shapeColor = "red";
+    var Ball1Position = database.ref("ball/position");
+    Ball1Position.on("value",readPosition, showError);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref("ball/position").set({
+        "x": position.x + x,
+        "y": position.y + y
+    })
+}
+
+function readPosition(){
+    position = data.val();
+    Ball1.x = position.x;
+    Ball1.y = position.y;
+}
+
+function showError(){
+    console.log("Error in writing to the database");
 }
